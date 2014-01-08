b535ab0043d9dded8a4ab6bfc6182d5acfad0a72
Apprentice <apprentice@dbc29.local>
2014-01-08 15:10:35 -0800

    Release 0: human algorithm pseudocode

---
+++ anagram.rb
@@ -0,0 +1,10 @@
+# Pseudocode ---
+
+# input: word, compare word
+#   check to see if first letter in word is in compare word
+#   if it is cross that letter out
+#   repeat with all letters in word
+#   if there is a match for all letters in word and all letters are crossed off
+#   in compare word the you got yourself an anagram
+
+
