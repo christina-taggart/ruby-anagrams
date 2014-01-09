e1326480728774ea133f3290608207a6fd4a49b0
Apprentice <apprentice@dbc05.local>
2014-01-08 17:14:22 -0800

    Release 2: Canonical Version

--- is_anagram.rb
+++ is_anagram.rb
@@ -15,6 +15,14 @@ def is_anagram?(word1, word2)
   end
 end
 
+def canonical(word)
+  word.downcase.split('').sort.join
+end
+
+def is_anagram?(word1, word2)
+  canonical(word1) == canonical(word2)
+end
+
 p is_anagram?('cinema', 'iceman') # => true
 p is_anagram?('iceman', 'cinema') # => true
 
