0edf123a278769c0298ce9a377124017c2d4480a
Apprentice <apprentice@dbc02.local>
2014-01-08 17:02:43 -0800

    Release 2 completed

--- anagram.rb
+++ anagram.rb
@@ -1,9 +1,15 @@
+def canonical(word)
+  word.split("").sort.join("")
+end
+
 def is_anagram?(word1,word2)
   letters1 = word1.split("")
   letters2 = word2.split("")
   letters1.sort! == letters2.sort!
 end
 
-p is_anagram?("dog","god") == true
+p is_anagram?(canonical("dog"),canonical("god")) == true
 p is_anagram?("dog", "good") == false
 p is_anagram?("cinema", "iceman") == true
+
+p canonical("spencer")
