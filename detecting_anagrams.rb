6919f2165ed477db97b5c128538721b7b60dd32c
Apprentice <apprentice@dbc08.local>
2014-01-08 18:43:51 -0800

    Release 1

--- detecting_anagrams.rb
+++ detecting_anagrams.rb
@@ -1,9 +1,9 @@
 def is_anagram?(word1, word2)
-  word1_array = word1.split(//)
-  word2_array = word1.split(//)
-  word1_array == word2_array
-
+  # word1_array = word1.split(//)
+  # word2_array = word2.split(//)
+  # word1_array == word2_array
+  word1.downcase.split(//).sort == word2.downcase.split(//).sort
 end
 
-p is_anagram?('cinema', 'ice git man') == true # => true
+p is_anagram?('cinema', 'Iceman') == true # => true
 p is_anagram?('iceman', 'cinema')  == true # => true
